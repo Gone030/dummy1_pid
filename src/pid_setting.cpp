16f296e2e0a0eea8cb40e4905f972719db20adf4
Gone030 <gjkjw_030@naver.com>
2023-01-03 22:29:25 +0900

    pid setting

--- src/pid_setting.cpp
+++ src/pid_setting.cpp
@@ -4,18 +4,21 @@
 #include <string>
 
 #include "rclcpp/rclcpp.hpp"
-#include "dummy1_msg/msg/pid.hpp"
+#include "std_msgs/msg/float64.hpp"
 
 using namespace std::chrono_literals;
 
 class PIDsetting : public rclcpp::Node {
     public:
-        PIDsetting() : Node("PID_setting"), count_(0) {
-            publisher_ = this -> create_publisher < dummy1_msg::msg::PID > ("pid_topic", 10);
+        PIDsetting() : Node("PID_setting"){
+            publisher_p = this -> create_publisher < std_msgs::msg::Float64 > ("p_vel", 10);
+            publisher_i = this -> create_publisher < std_msgs::msg::Float64 > ("i_vel", 10);
+            publisher_d = this -> create_publisher < std_msgs::msg::Float64 > ("d_vel", 10);
         }
     private:
-        rclcpp::Publisher< dummy1_msg::msg::PID >::SharedPtr publisher_;
-        size_t count_;
+        rclcpp::Publisher< std_msgs::msg::Float64 >::SharedPtr publisher_p;
+        rclcpp::Publisher< std_msgs::msg::Float64 >::SharedPtr publisher_i;
+        rclcpp::Publisher< std_msgs::msg::Float64 >::SharedPtr publisher_d;
 };
 
 int main(int argc,char *argv[])
